3d12fade18fb430237f28ab5c9315a26f265129b
Yusent Chig <yusent@protonmail.com>
2016-09-09 17:55:57 -0700

    Move problem8 description to README.md file

---
+++ problem8/README.md
@@ -0,0 +1,26 @@
+Largest product in a series
+======
+Find the greatest product of K consecutive digits in the N digit number.
+
+##### Input Format:
+First line contains T that denotes the number of test cases.
+First line of each test case will contain two integers N & K.
+Second line of each test case will contain a N digit integer.
+
+##### Output Format:
+Print the required answer for each test case.
+
+##### Sample Input:
+```
+2
+10 5
+3675356291
+10 5
+2709360626
+```
+
+##### Sample Output:
+```
+3150
+0
+```
--- problem8/haskell/Main.hs
+++ problem8/haskell/Main.hs
@@ -1,26 +1,3 @@
-{-|
-  Find the greatest product of K consecutive digits in the N digit number.
-
-  Input Format:
-  First line contains T that denotes the number of test cases.
-  First line of each test case will contain two integers N & K.
-  Second line of each test case will contain a N digit integer.
-
-  Output Format:
-  Print the required answer for each test case.
-
-  Sample Input:
-  2
-  10 5
-  3675356291
-  10 5
-  2709360626
-
-  Sample Output:
-  3150
-  0
--}
-
 import Control.Monad (replicateM_)
 import Solution      (subSetProducts)
 
